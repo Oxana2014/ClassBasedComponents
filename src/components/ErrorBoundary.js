50c99280391ef876ceb2235639d39ad74bbdc8c4
Oksana Kormilina <keysb@mail.ru>
2024-01-29 13:04:19 +0400

    Add error boundary class

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import classes from "./ErrorBoundary.module.css";
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+    };
+  }
+  componentDidCatch(error) {
+    console.log(error);
+    this.setState({ hasError: true });
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className={classes["error-message"]}>
+          <p >Something went wrong</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -3,6 +3,7 @@ import { Component, Fragment} from "react";
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
 import UsersContext from "../store/users-context";
+import ErrorBoundary from './ErrorBoundary'
 
 
 class UserFinder extends Component {
@@ -38,7 +39,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
+        <ErrorBoundary>
         <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
